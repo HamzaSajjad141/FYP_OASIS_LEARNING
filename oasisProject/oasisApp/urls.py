d1a1f66022b258e64847e2b4bf6059d8820c4b38
HamzaSajjad141 <hamzasajjad1277@gmail.com>
2023-09-09 16:51:49 +0500

    Commiyt Paths

--- oasisProject/oasisApp/urls.py
+++ oasisProject/oasisApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   path('', views.func),
+   path('', views.Home,name='Home'),
+   path('Login/', views.Login,name='Login'),
+   path('RegisterPage/', views.Register,name='Register'),
    
 ]
--- oasisProject/oasisApp/views.py
+++ oasisProject/oasisApp/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 
-def func(request):
-    return HttpResponse("HELLO THERE ")
+def Home(request):
+    return HttpResponse("Home Page ")
+
+
+def Login(request):
+    return HttpResponse("Login Page")
+
+
+def Register(request):
+    return HttpResponse("SignUp Page")
