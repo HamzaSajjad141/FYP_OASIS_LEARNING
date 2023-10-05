ba64df328016482525808800dc3ebe124d4a5834
Zaeem Muhammad Yaseen <zaeem.m.yaseen@gmail.com>
2023-10-05 23:03:02 +0500

    edited the liama.py file

--- coding/liama.py
+++ coding/liama.py
@@ -1,10 +1,7 @@
 import os 
 import replicate
-
 os.environ['REPLICATE_API_TOKEN'] = "r8_FhEaFUEUUwFglCHK4j2pFQJjz6EVGPT2LUNWR"
-
 prompt_input = input("Enter you question: ")
-
 output = replicate.run(
     "meta/llama-2-7b-chat:8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e",
     input={
@@ -15,13 +12,9 @@ output = replicate.run(
         "repitition_penalty": 1
     }
 )
-
 full_response = ""
-
 for item in output:
     full_response += item
-
 print(full_response)
-
 print("\n\n")
 
