7256f9c2518d51a1e2e8f6a593b0255f4fce00b9
Hamza Sajjad <hamzasajjad1277@gmail.com>
2024-03-27 01:00:43 +0500

    Create Databases and Superuser(Admin)

--- oasisProject/oasisApp/models.py
+++ oasisProject/oasisApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone  # Import timezone
 
-# Create your models here.
+class Complaint(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    status = models.CharField(max_length=100, default='Pending')
+    respond = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Complaint by {self.user.username}"
+
+class History(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_url = models.URLField()
+    query_text = models.TextField()
+    translated_text = models.TextField()
+    video_url = models.URLField()
+    date = models.DateTimeField(default=timezone.now)  # Add this field
+
+    def __str__(self):
+        return f"History for {self.user.username}"
--- virt/Scripts/activate.bat
+++ virt/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\FasTdevGIT\FYP_OASIS_LEARNING\virt
+set VIRTUAL_ENV=D:\GITHUB\FYP_OASIS_LEARNING\virt
 
 if not defined PROMPT set PROMPT=$P$G
 
