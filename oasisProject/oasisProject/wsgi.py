f39b49c5234eb964decf36286b5668cb8353420f
Zaeem Muhammad Yaseen <zaeem.m.yaseen@gmail.com>
2024-05-15 01:33:10 +0500

    added necessary files for deployment (gitignore, requirements.txt)

--- oasisProject/oasisProject/wsgi.py
+++ oasisProject/oasisProject/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for oasisProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oasisProject.settings')
+settings_module = 'oasisProject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'oasisProject.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
